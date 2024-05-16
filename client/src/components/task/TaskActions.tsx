88b922249aec1c2a8239bf04c152e92da3fafad9
harry18121911 <harrytheripper1812@hotmail.com.ar>
2024-05-16 19:11:57 -0300

    Github submit

--- client/src/components/task/TaskActions.tsx
+++ client/src/components/task/TaskActions.tsx
@@ -1,37 +1,49 @@
 import { Pressable, TextInput } from 'react-native'
 import { Box, Text } from '../utils/theme'
 import { useState } from 'react'
-import { ITaskRequest } from '../../types'
+import { ICategory, ITaskRequest } from '../../types'
 import { format, isToday } from "date-fns"
-type TaskActionsProps ={
+import useSWR from 'swr'
+import Loader from '../shared/Loader'
+
+type TaskActionsProps = {
   categoryId: string
 }
 
-const todaysISODate= new Date().toISOString()
+const todaysISODate = new Date().toISOString()
 
 
-const TaskActions = ({categoryId} :TaskActionsProps) => {
+const TaskActions = ({ categoryId }: TaskActionsProps) => {
   const [newTask, setNewTask] = useState<ITaskRequest>({
-    categoryId:categoryId,
-    date:todaysISODate,
+    categoryId: categoryId,
+    date: todaysISODate,
     isCompleted: false,
-    name:""
+    name: ""
   })
 
+  const [currentCategory, setCurrentCategory] = useState<ICategory>()
+
+  const {data:categories, isLoading} = useSWR<ICategory[]>("category")
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  const selectedCategory = categories?.find((_category)=> _category._id ===categoryId)
 
   return (
-    <Box bg='lightGray' px='4' py='4' mx='4'mt='4' borderRadius='rounded-5xl'>
+    <Box bg='lightGray' px='4' py='4' mx='4' mt='4'  borderRadius='rounded-5xl' flexDirection='row'>
       <TextInput placeholder='Create a new task' style={{
-        fontSize:20
+        fontSize: 20
       }}
-        maxLength={36}  
+        maxLength={36}
         textAlign='center'
         value={newTask.name}
-        onChangeText={(text)=>{
-          setNewTask((prev) =>{
+        onChangeText={(text) => {
+          setNewTask((prev) => {
             return {
               ...prev,
-              name:text,
+              name: text,
             }
           })
         }}
@@ -41,11 +53,27 @@ const TaskActions = ({categoryId} :TaskActionsProps) => {
         <Pressable>
           <Box flexDirection='row' alignContent='center' bg='white' p='2' borderRadius='rounded-xl'>
             <Text>
- {isToday(new Date(newTask.date))
-                  ? "Today"
-                  : format(new Date(newTask.date), "yyyy-MM-dd")}           
+              {isToday(new Date(newTask.date))
+                ? "Today"
+                : format(new Date(newTask.date), "yyyy-MM-dd")}
 
             </Text>
+          </Box>
+        </Pressable>
+        <Box height={12} />
+        <Pressable>
+          <Box bg='white' flexDirection='row' alignItems='center' p='2' borderRadius='rounded-xl' ml='4'>
+            <Box
+              width={12}
+              height={12}
+              borderRadius='rounded'
+              borderWidth={2}
+              mr='1'
+              style={{
+                borderColor:selectedCategory?.color.code 
+              }}
+            ></Box>
+              <Text style={{color:selectedCategory?.color.code}}>{selectedCategory?.name}</Text>
           </Box>
         </Pressable>
       </Box>
